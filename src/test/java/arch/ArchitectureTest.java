68a4a449ca005b2fb07fc7c48693b262251e9273
Anton <pestrikv1337@gmail.com>
2023-11-02 12:17:51 +0300

    AT-29.Added architecture tests

--- src/test/java/arch/ArchitectureTest.java
+++ src/test/java/arch/ArchitectureTest.java
@@ -6,9 +6,13 @@ import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
 
-class ArchitectureTest {
+public class ArchitectureTest {
+    private static final String FW_LAYER = "..sd.api.fw..";
+    private static final String APP_LAYER = "..sd.api.app..";
+    private static final String API_LAYER = "..sd.api.app.api..";
+    private static final String IMPL_LAYER = "..sd.api.app.impl..";
+    private static final String DOMAIN_LAYER = "..domain..";
     private JavaClasses importedClasses;
 
     @Before
@@ -18,42 +22,39 @@ class ArchitectureTest {
     }
 
     @Test
-    @DisplayName("Пакет fw имеет доступ только к пакету app")
     public void fwShouldOnlyAccessApp() {
         ArchRule rule = ArchRuleDefinition
                 .classes()
                 .that()
-                .resideInAnyPackage("..sd.api.fw..")
+                .resideInAnyPackage(FW_LAYER)
                 .should()
                 .onlyAccessClassesThat()
-                .resideInAnyPackage("..security..", "..aspect..", "..spring..", "org.springframework..", "org.aspectj..", "lombok..", "jakarta..", "org.redisson..", "com.fasterxml..", "io.swagger..", "java..", "org.slf4j..", "..sd.api.app..");
+                .resideInAnyPackage("..security..", "..aspect..", "..spring..", "org.springframework..", "org.aspectj..", "lombok..", "jakarta..", "org.redisson..", "com.fasterxml..", "io.swagger..", "java..", "org.slf4j..", APP_LAYER);
         rule.check(importedClasses);
     }
 
     @Test
-    @DisplayName("Пакет api имеет доступ только к пакету domain")
     public void apiShouldOnlyAccessDomain() {
         ArchRule rule = ArchRuleDefinition
                 .classes()
                 .that()
-                .resideInAPackage("..sd.api.app.api")
+                .resideInAPackage(API_LAYER)
                 .should()
                 .onlyAccessClassesThat()
-                .resideInAnyPackage("..api.app.api..", "..api.app.impl..", "..domain..", "java..", "org.springframework..", "lombok..", "com.fasterxml.jackson..");
+                .resideInAnyPackage(API_LAYER, IMPL_LAYER, DOMAIN_LAYER, "java..", "org.springframework..", "lombok..", "com.fasterxml.jackson..");
         rule.check(importedClasses);
     }
 
     @Test
-    @DisplayName("Пакет domain не имеет доступ к внешним пакетам")
     public void domainShouldNotAccessOtherPackages() {
         ArchRule rule = ArchRuleDefinition
                 .classes()
                 .that()
-                .resideInAnyPackage("..domain..")
+                .resideInAnyPackage(DOMAIN_LAYER)
                 .should()
                 .onlyAccessClassesThat()
                 .resideInAnyPackage(
-                        "..domain..", "java..", "org.hibernate..");
+                        DOMAIN_LAYER, "java..", "org.hibernate..");
         rule.check(importedClasses);
     }
 }
