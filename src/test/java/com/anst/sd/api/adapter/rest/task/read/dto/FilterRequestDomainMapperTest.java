6564228e938602700b631719a48ac9c10f325d66
eridium <pestrikv1337@gmail.com>
2023-12-29 15:55:51 +0700

    AT-16.Mapper tests

---
+++ src/test/java/com/anst/sd/api/adapter/rest/task/read/dto/FilterRequestDomainMapperTest.java
@@ -0,0 +1,27 @@
+package com.anst.sd.api.adapter.rest.task.read.dto;
+
+import com.anst.sd.api.AbstractUnitTest;
+import com.anst.sd.api.adapter.rest.task.read.dto.FilterRequestDomainMapper;
+import com.anst.sd.api.adapter.rest.task.read.dto.TaskFilterRequestDto;
+import com.anst.sd.api.app.api.task.TaskFilter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+class FilterRequestDomainMapperTest extends AbstractUnitTest {
+    private FilterRequestDomainMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mapper = Mappers.getMapper(FilterRequestDomainMapper.class);
+    }
+
+    @Test
+    void mapToDomain() {
+        TaskFilterRequestDto source = readFromFile("/FilterRequestDomainMapperTest/filterRequest.json", TaskFilterRequestDto.class);
+
+        TaskFilter domain = mapper.mapToDomain(source);
+
+        assertEqualsToFile("/FilterRequestDomainMapperTest/filterRequestDomain.json", domain);
+    }
+}
