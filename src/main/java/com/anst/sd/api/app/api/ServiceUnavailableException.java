dd19d5de04083fc56bbc4c98da5bb2ed2af01eaa
strelnikov <for.back.school@yandex.ru>
2024-05-10 17:22:02 +0300

    AT-77 Реализация шедулера для отправления уведомления через tg-bot.

--- src/main/java/com/anst/sd/api/app/api/ServiceUnavailableException.java
+++ src/main/java/com/anst/sd/api/app/api/ServiceUnavailableException.java
@@ -1,8 +1,5 @@
 package com.anst.sd.api.app.api;
 
-/**
- * Сервис недоступен
- */
 public class ServiceUnavailableException extends RuntimeException {
     public ServiceUnavailableException(Exception e) {
         super(e);
