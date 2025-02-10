589409d35a951b8bb8447e8325e50c03d8d81245
strelnikov <for.back.school@yandex.ru>
2025-02-10 19:11:53 +0300

    AT-104 2FA

--- src/test/java/it/AbstractIntegrationTest.java
+++ src/test/java/it/AbstractIntegrationTest.java
@@ -58,7 +58,7 @@ public abstract class AbstractIntegrationTest {
     @Autowired
     protected RedissonClient redissonClient;
     @Autowired
-    private UserJpaRepository userJpaRepository;
+    protected UserJpaRepository userJpaRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
     @Autowired
