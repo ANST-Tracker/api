fa3a76787d1ceeaef05d925ee38ee7b7be1d9c4c
Eridium <76278533+eridiumprojects@users.noreply.github.com>
2025-03-12 00:27:01 +0300

    Reformat sql script

--- src/main/resources/db/migration/V006__create_table_abstract_task.sql
+++ src/main/resources/db/migration/V006__create_table_abstract_task.sql
@@ -1,17 +1,17 @@
 create table public.abstract_task
 (
     id              uuid primary key,
-    simple_id  varchar not null,
+    simple_id       varchar not null,
     name            varchar not null,
     description     varchar,
     type            varchar not null,
     priority        varchar not null,
-    status     varchar not null,
+    status          varchar not null,
     story_points    bigint,
     assignee_id     uuid,
     reviewer_id     uuid,
-    creator_id uuid    not null,
-    project_id uuid    not null,
+    creator_id      uuid    not null,
+    project_id      uuid    not null,
     due_date        varchar,
     order_number    numeric(13, 10),
     time_estimation jsonb
