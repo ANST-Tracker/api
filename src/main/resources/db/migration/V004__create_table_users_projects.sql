2c0840cb6068c89d0aedb077d5ed01dd71130cd9
Pestrikov Anton <pestrikv1337@gmail.com>
2025-05-15 11:41:28 +0300

    Migration changed

--- src/main/resources/db/migration/V004__create_table_users_projects.sql
+++ src/main/resources/db/migration/V004__create_table_users_projects.sql
@@ -3,7 +3,7 @@ create table public.users_projects
     id              uuid primary key,
     user_id         uuid        not null,
     project_id      uuid        not null,
-    permission_code varchar     not null,
+    permission_code varchar,
     created_at      timestamptz not null,
     updated_at      timestamptz
 );
